7529df5c6d282335c3fe90fb27ea7205f265e68e
ronaldo <ronaldo_fernandes_8@hotmail.com>
2018-07-08 13:27:57 -0300

    primeiro commit

---
+++ Rede/Rede/Network.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rede
+{
+    class Network
+    {
+        private int qtdElementos;
+        private int qtdArestas = 0;
+        private bool contem ;
+
+        private Dictionary<int, List<int>> Elemento = new Dictionary<int, List<int>>();
+
+        public int QtdElementos
+        {
+            get { return qtdElementos; }
+            set { qtdElementos = value; }
+        }
+
+        public int QtdArestas
+        {
+            get { return qtdArestas; }
+            set { qtdArestas = value; }
+        }
+
+        public bool Contem
+        {
+            get { return contem; }
+            set { contem = value; }
+        }
+        
+        // criar grafo informa quantidade de elementos 
+        public Network(int qtdElementos)
+        {
+
+            this.QtdElementos = qtdElementos;
+             
+            Console.WriteLine("Deseja preencher automaticamente? Sim = S Não = N");
+            string resposta = Console.ReadLine();
+
+            char resp = char.Parse(resposta);
+
+            while (resp != 'N' && resp != 'S' && resp != 'n' && resp != 's')
+            {
+                Console.WriteLine("Opcao Invalida insira S ou N");
+                resposta = Console.ReadLine();
+                resp = char.Parse(resposta);
+            }
+            switch (resp)
+            {
+                case  'S':
+                case  's':
+                    for (int i = 1; i < (qtdElementos + 1); i++) Elemento.Add(i, new List<int>());
+                    break;
+                case 'N':
+                case 'n':
+                    for (int i = 1; i < (qtdElementos + 1); i++) {
+                        Console.Write($"Valor do Elemento nº {i}: ");
+
+                        var numero = Console.ReadLine();
+                        while (!char.IsNumber(numero, 0))
+                        {
+                            Console.WriteLine("Valor precisa ser numerico");
+                            Console.Write($"Valor do Elemento nº {i}: ");
+                            numero = Console.ReadLine();
+                        }
+                     
+                        Elemento.Add(Convert.ToInt32(numero), new List<int>());
+                    }
+                    break;
+                
+            }
+            
+        }
+
+        // verifica se existe os elementos para poder formar a ligação
+        public bool verifica_elementos(int ele1, int ele2)
+        {
+            bool existe = false;
+            foreach(KeyValuePair<int, List<int>> i in Elemento)
+            {
+                if (Elemento.ContainsKey(ele1) && (Elemento.ContainsKey(ele2)))
+                    existe = true;
+                else
+                    existe = false;
+            }
+
+
+            return existe;
+        }
+        //conecta dois elementos 
+        public void connect(int val1, int val2)
+        {
+            
+
+            foreach (KeyValuePair<int, List<int>> i in Elemento)
+            {
+                if (verifica_elementos(val1, val2))
+                {
+
+                    if (val1.Equals(i.Key))
+                    {
+                        if (!i.Value.Contains(val2))
+                        {
+                            i.Value.Add(val2);
+                            qtdArestas++;
+                        }
+                    }
+
+                    if (val2.Equals(i.Key))
+                    {
+                        if (!i.Value.Contains(val1))
+                            i.Value.Add(val1);
+                    }
+                    
+                }else
+                {
+                    Console.WriteLine("Um ou todos os elementos nao existem");
+                   
+                    break;
+                }
+                
+
+            }
+        }
+
+        // Imprime elementos
+        public void Imprime()
+        {
+            Console.Write("\nElementos: ");
+            foreach (KeyValuePair<int, List<int>> i in Elemento)
+            {
+                Console.Write( i.Key + "  ");
+            }
+            Console.WriteLine(" ");
+        }
+
+        // Imprime ligaçoes
+        public void ImprimeConexoes()
+        {
+            foreach (KeyValuePair<int, List<int>> i in Elemento)
+            {
+                Console.Write("\nLigações: " + i.Key + "  ");
+                foreach (int j in (i.Value as List<int>)) Console.Write(j + "  ");
+            }
+            Console.WriteLine(" ");
+        }
+        // verifica se existe caminho entre 2 elementos
+        public bool query(int val1, int val2)
+        {
+            List<int> path = null;
+            var antes = new Dictionary<int, int>();
+            var pesos = new Dictionary<int, int>();
+            var nodes = new List<int>();
+
+            //zerando o peso do item inicial e colocando max nos outros
+            foreach (var item in Elemento)
+            {
+                if (item.Key == val1)
+                {
+                    pesos[item.Key] = 0;
+                }
+                else
+                {
+                    pesos[item.Key] = int.MaxValue;
+                }
+
+                nodes.Add(item.Key);
+            }
+
+            while (nodes.Count != 0)
+            {
+                //realiza o sort de acordo com a menor (chave - peso).
+                // ex.: a(2,2) b(2, 2.71)  neste caso escolhe o b.
+                nodes.Sort((x, y) => pesos[x] - pesos[y]);
+
+                var menor = nodes[0];
+                nodes.Remove(menor);
+                // quando o menor for o valor procurado. 
+                if (menor == val2)
+                {
+                    path = new List<int>();
+                    while (antes.ContainsKey(menor))
+                    {
+                        path.Add(menor);
+                        menor = antes[menor];
+                    }
+                    if (path.Count == 0)
+                    {
+                        Contem = false;
+                    }
+                    else { Contem = true; }
+                        
+                   break;
+                    
+                }
+
+                if (pesos[menor] == int.MaxValue)
+                {
+                    break;
+                }
+                //pegando visinhos do elemento e atualizando pesos.
+                foreach (var visinho in Elemento[menor])
+                {
+                    var atualiza = pesos[menor] + visinho;
+                    if (atualiza < pesos[visinho])
+                    {
+                        pesos[visinho] = atualiza;
+
+                        antes.Add(visinho, menor);
+                    }
+                }
+            }
+
+            return Contem;
+        }
+
+    }
+}
---
+++ Rede/Rede/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rede
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\t\t**********************************************");
+            Console.WriteLine("\t\t*** Programa conexao de pontos no Conjunto ***");
+            Console.WriteLine("\t\t**********************************************\n");
+            
+            Console.Write("Insira a quantidade de Elementos:  ");
+            var entrada = Console.ReadLine();
+            // controle da entrada de quantidades
+            while (!char.IsNumber(entrada, 0))
+            {
+                Console.WriteLine("Opção precisa ser numerica");
+                Console.Write("Insira a quantidade de Elementos:  ");
+
+                entrada = Console.ReadLine();
+            }
+
+            int resposta =Convert.ToInt32(entrada);
+            Network net = new Network(resposta);
+
+            Console.Write("\nImprime Elementos = 1 \nCriar conexão     = 2 \nVerificaConexao   = 3"
+                   + "\nImprimeConexoes   = 4 \nEncerrar          = 5\n \nSelecione a opção: ");
+
+            var opt = Console.ReadLine();
+            // controle de opcoes
+            while (!char.IsNumber(opt, 0))
+            {
+                Console.WriteLine("Opção precisa ser numerica");
+                Console.Write("\nImprime Elementos = 1 \nCriar conexão     = 2 \nVerificaConexao   = 3"
+              + "\nImprimeConexoes   = 4 \nEncerrar          = 5\n \nSelecione a opção: ");
+
+                opt = Console.ReadLine();
+            }
+
+            int opcao = Convert.ToInt32(opt);
+
+            while (opcao != 5) { 
+                switch (opcao)
+                {
+                    case 1:
+                        net.Imprime();
+                        break;
+                    case 2:
+                        string respost = null;
+                        char resp = ' ';
+
+                        do
+                        {
+                            Console.Write("Valor1 :");
+                            var numero1 = Console.ReadLine();
+                            while (!char.IsNumber(numero1, 0))
+                            {
+                                Console.WriteLine("Valor precisa ser numerico");
+                                Console.Write("Valor1 :");
+                                numero1 = Console.ReadLine();
+                            }
+                            int valor1 = Convert.ToInt32(numero1);
+
+                            Console.Write("Valor2 :");
+
+                            var numero2 = Console.ReadLine();
+                            while (!char.IsNumber(numero2, 0))
+                            {
+                                Console.WriteLine("Valor precisa ser numerico");
+                                Console.Write("Valor2 :");
+                                numero2 = Console.ReadLine();
+                            }
+
+                            int valor2 = Convert.ToInt32(numero2);
+
+                            net.connect(valor1, valor2);
+                            Console.Write("Deseja Criar Nova Conexão? Sim=S Não = N  ");
+                            respost = Console.ReadLine();
+                            resp = char.Parse(respost);
+
+                            while (resp != 'N' && resp != 'S' && resp != 'n' && resp != 's')
+                            {
+                                Console.Write("Opcao Invalida insira S ou N ");
+                                respost = Console.ReadLine();
+                                resp = char.Parse(respost);
+                            }
+
+                        } while (resp == 'S' || resp == 's');
+
+                        break;
+                    case 3:
+                        Console.Write("Insira os valores a verificar se existe conexão\n");
+                        Console.Write("Valor1 :");
+                        var valo = Console.ReadLine();
+                        while (!char.IsNumber(valo, 0))
+                        {
+                            Console.WriteLine("Valor precisa ser numerico");
+                            Console.Write("Valor1 :");
+                            valo = Console.ReadLine();
+                        }
+
+                        int valo1 = Convert.ToInt32(valo);
+
+                        Console.Write("Valor2 :");
+
+                        valo = Console.ReadLine();
+                        while (!char.IsNumber(valo, 0))
+                        {
+                            Console.WriteLine("Valor precisa ser numerico");
+                            Console.Write("Valor2 :");
+                            valo = Console.ReadLine();
+                        }
+
+                        int valo2 = Convert.ToInt32(valo);
+
+                        if (net.query(valo1, valo2))
+                        {
+                            Console.WriteLine("\nEstá conectado");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nNão está conectado");
+                        }
+
+                        break;
+                    case 4:
+                        net.ImprimeConexoes();
+                        break;
+                    default:
+                        Console.WriteLine("\nOpção Invalida");
+                        break;
+
+                }
+                Console.Write("\nImprime Elementos = 1 \nCriar conexão     = 2 \nVerificaConexao   = 3"
+                       + "\nImprimeConexoes   = 4 \nEncerrar          = 5\n \nSelecione a opção: ");
+
+                opt = Console.ReadLine();
+
+                while (!char.IsNumber(opt, 0))
+                {
+                    Console.WriteLine("Opção precisa ser numerica");
+                    Console.Write("\nImprime Elementos = 1 \nCriar conexão     = 2 \nVerificaConexao   = 3"
+                  + "\nImprimeConexoes   = 4 \nEncerrar          = 5\n \nSelecione a opção: ");
+
+                    opt = Console.ReadLine();
+                }
+
+                opcao = Convert.ToInt32(opt);
+
+            }
+            
+        }
+    }
+}
